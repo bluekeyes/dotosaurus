876671811e81b610bb03bcec97d721689c838786
Billy Keyes <bluekeyes@gmail.com>
2017-06-26 14:48:09 -0700

    [zsh] Install github/hub

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -3,6 +3,7 @@ zplug "lukechilds/zsh-nvm"
 # load commands
 zplug "stedolan/jq", from:gh-r, as:command, rename-to:jq
 zplug "BurntSushi/ripgrep", from:gh-r, as:command, rename-to:rg
+zplug "github/hub", from:gh-r, as:command, rename-to:hub
 
 # load custon configuration
 zplug "${DOTOSAURUS}/zsh/plugins", from:local
