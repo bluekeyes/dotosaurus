e543195823dfb2de3712a06c961cd4315ebe7e2d
Billy Keyes <bluekeyes@gmail.com>
2017-08-29 11:17:05 -0700

    [git/zsh] alias hub to git

--- zsh/plugins/aliases.zsh
+++ zsh/plugins/aliases.zsh
@@ -16,3 +16,6 @@ alias grep="grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}"
 # other
 alias less="less -S"
 alias gradled="gradlef --daemon"
+
+# add github actions to 'git'
+alias git="hub"
