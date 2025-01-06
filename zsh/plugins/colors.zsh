98ff494e0bf375037ae7326f0c55c894fbadb058
Billy Keyes <commits@billykeyes.dev>
2025-01-06 12:52:10 -0800

    [zsh] Only run dircolors if the command exists
    This does not exist on macOS by default but zplug was hiding the
    failure message.

--- zsh/plugins/colors.zsh
+++ zsh/plugins/colors.zsh
@@ -3,5 +3,7 @@ source ${DOTOSAURUS}/colors/base16-tomorrow/shell.sh
 
 # enable ls colors
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
-eval "$(dircolors ${DOTOSAURUS}/colors/dark.256.dircolors)"
+if command -v dircolors >/dev/null 2>&1; then
+    eval "$(dircolors ${DOTOSAURUS}/colors/dark.256.dircolors)"
+fi
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
